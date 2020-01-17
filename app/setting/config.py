cb61e5f564923124fae6d0f116d69857b63e8237
cocoa <hotococoa@qq.com>
2020-01-17 17:11:16 +0800

    index image

--- app/setting/config.py
+++ app/setting/config.py
@@ -78,7 +78,7 @@ image_site = "http://hotobun.gitee.io/hexo/"
 
 # index_image = 'img/index.png'
 # index_image = 'https://s2.ax1x.com/2020/01/17/lxgfxO.jpg'
-index_image = "https://imgchr.com/i/lzICLV"
+index_image = "https://s2.ax1x.com/2020/01/17/lzICLV.jpg"
 
 if __name__ == "__main__":
     print(archive_path)
